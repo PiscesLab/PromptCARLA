60e40868159d1493903c6b362c376e8cd2742cbc
waphong <waphong@gpu-node.metiscity.pisceslabsd-pg0.wisc.cloudlab.us>
2026-03-13 17:08:20 -0500

    remove unnecessary backup folder

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["128.105.144.61:3000", "localhost:3000"],
+    },
+  },
 };
 
 export default nextConfig;
